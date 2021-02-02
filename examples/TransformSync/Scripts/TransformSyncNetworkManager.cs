ee8f814a7716984c5fe6b75a708d4f1d7f99ed6d
James Frowen <jamesfrowendev@gmail.com>
2021-02-02 01:13:25 +0000

    updating example

--- examples/TransformSync/Scripts/TransformSyncNetworkManager.cs
+++ examples/TransformSync/Scripts/TransformSyncNetworkManager.cs
@@ -1,25 +1,69 @@
 using Mirror;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace JamesFrowen.PositionSync.Example
 {
     public class TransformSyncNetworkManager : NetworkManager
     {
+        [Header("References")]
+        [SerializeField] SyncPositionSystem system;
+
         [Header("Moving objects")]
         [SerializeField] int cubeCount = 10;
         [SerializeField] GameObject cubePrefab;
+        [SerializeField] bool showGUI = true;
+        [SerializeField] Rect guiRect = new Rect(100, 0, 200, 200);
 
+        List<GameObject> cubes = new List<GameObject>();
         public override void OnStartClient()
         {
-            ClientScene.RegisterPrefab(cubePrefab);
+            this.system.RegisterHandlers();
+            ClientScene.RegisterPrefab(this.cubePrefab);
         }
 
         public override void OnStartServer()
         {
-            for (int i = 0; i < cubeCount; i++)
+            this.system.RegisterHandlers();
+            for (var i = 0; i < this.cubeCount; i++)
             {
-                GameObject clone = Instantiate(cubePrefab);
-                NetworkServer.Spawn(clone);
+                this.SpawnNewCube();
+            }
+        }
+
+        private void SpawnNewCube()
+        {
+            var clone = Instantiate(this.cubePrefab);
+            NetworkServer.Spawn(clone);
+            this.cubes.Add(clone);
+        }
+
+        public override void OnStopClient()
+        {
+            this.system.RegisterHandlers();
+        }
+        public override void OnStopServer()
+        {
+            this.system.RegisterHandlers();
+        }
+
+
+        private void OnGUI()
+        {
+            if (!this.showGUI) { return; }
+            if (!NetworkServer.active) { return; }
+
+            using (new GUILayout.AreaScope(this.guiRect))
+            {
+                GUILayout.Label($"Cube Count: {this.cubes.Count}");
+                if (GUILayout.Button("Add Cube"))
+                {
+                    this.SpawnNewCube();
+                }
+                if (GUILayout.Button("Remove Cube"))
+                {
+                    if (this.cubes.Count > 0) { this.cubes.RemoveAt(this.cubes.Count - 1); }
+                }
             }
         }
     }
