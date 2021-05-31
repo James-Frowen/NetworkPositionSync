af28fe0c1d36f412cafbb47bc8829ce0c0a71b4a
James Frowen <jamesfrowendev@gmail.com>
2021-05-31 22:38:30 +0100

    chaning error to warning
    mirror wont add define unless there are no warnings, therefore we can not have an error here

--- source/AssemblyInfo.cs
+++ source/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 [assembly: AssemblyVersion("1.0.0")]
 
 #if !MIRROR_35_0_OR_NEWER
-#error Only supports mirror version v35 or later
+#warning Only supports mirror version v35 or later
 #endif
 #if MIRROR_36_0_OR_NEWER
 #warning Latest supported version is v35
