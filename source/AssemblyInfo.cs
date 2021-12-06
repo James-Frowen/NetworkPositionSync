15f9a18d96760e0247c2e7af19769d5136558e8a
James Frowen <jamesfrowendev@gmail.com>
2021-12-06 23:26:18 +0000

    feat: coverting code to work with mirage
    BREAKING CHANGE: code no longer supports mirror

--- source/AssemblyInfo.cs
+++ source/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyVersion("3.0.0")]
 
 [assembly: InternalsVisibleTo("JamesFrowen.PositionSync.Tests.Runtime")]
