3401c6a06ca8df3dc4509328cedadc4a7f9a9b11
Matt Coburn <coburn@dandere.net>
2022-01-11 15:25:35 +1000

    Update InterpolationTime.cs

--- source/InterpolationTime.cs
+++ source/InterpolationTime.cs
@@ -30,25 +30,25 @@ using UnityEngine;
 namespace JamesFrowen.PositionSync
 {
     /// <summary>
-    /// Syncs time between server and client be receive regular message from server
+    /// Synchronizes time between server and client via regular messages between server and client.
     /// <para>Can be used for snapshot interpolation</para>
     /// </summary>
     /// <remarks>
-    /// This class will speed up or slow down Client time scale based on if it is ahead of behind the lastest server time
+	/// This class will speed up or slow down the client time scale, depending if it is ahead or behind the lastest server time.
     /// <para>
-    /// Every Update we add DeltaTime * TimeScale to client time
+    /// Every update we add DeltaTime * TimeScale to client time.
     /// </para>
     /// <para>
-    /// Every Update server sends message with its time<br/>
-    /// When client receives message it calculates difference between server time and local time<br/>
-    /// This difference is stored in a moving average so it is smoothed out
+    /// On the server, when an update is performed, the server will send a message back with its time.<br/>
+    /// When the client receives this message, it calculates the difference between server time and its own local time.<br/>
+    /// This difference is stored in a moving average, which is smoothed out.
     /// </para>
     /// <para>
-    /// If this difference is greater or less than a threshold then we speed up or slow down Client time scale<br/>
-    /// If difference is between threshold time is set back to normal scale
+    /// If the calculated difference is greater or less than a threshold then we adjust the client time scale by speeding up or slowing down.<br/>
+    /// If the calculated difference is between our defined threshold times, client time scale is set back to normal.
     /// </para>
     /// <para>
-    /// This Client time can then be used to snapshot interpolation using <c>InterpolationTime = ClientTime - Offset</c>
+    /// This client time can then be used to snapshot interpolation using <c>InterpolationTime = ClientTime - Offset</c>
     /// </para>
     /// <para>
     /// Some other implementations include the offset in the time scale calculations itself,
@@ -62,26 +62,24 @@ namespace JamesFrowen.PositionSync
     {
         static readonly ILogger logger = LogFactory.GetLogger<InterpolationTime>();
 
-
-
         bool intialized;
         /// <summary>
-        /// time client uses to interpolate
+        /// The time value that the client uses to interpolate
         /// </summary>
         float _clientTime;
         /// <summary>
-        /// Multiples deltaTime by this scale each frame
+        /// The client will multiply deltaTime by this scale time value each frame
         /// </summary>
         float clientScaleTime;
 
         readonly ExponentialMovingAverage diffAvg;
 
         /// <summary>
-        /// how much above goalOffset diff is allowed to go before changing timescale
+        /// How much above the goalOffset difference are we allowed to go before changing the timescale
         /// </summary>
         readonly float positiveThreshold;
         /// <summary>
-        /// how much below goalOffset diff is allowed to go before changing timescale
+        /// How much below the goalOffset difference are we allowed to go before changing the timescale
         /// </summary>
         readonly float negativeThreshold;
 
@@ -90,13 +88,14 @@ namespace JamesFrowen.PositionSync
         readonly float slowScale = 0.99f;
 
         /// <summary>
-        /// if new time and previous time are this far apart then reset client time
+        /// Is the difference between previous time and new time too far apart?
+		/// If so, reset the client time.
         /// </summary>
         readonly float _skipAheadThreshold;
 
         float _clientDelay;
 
-        // debug
+        // Used for debug purposes. Move along...
         float _latestServerTime;
 
         /// <summary>
@@ -108,7 +107,7 @@ namespace JamesFrowen.PositionSync
             get => _clientTime;
         }
         /// <summary>
-        /// Last time Received by server
+        /// Returns the last time received by the server
         /// </summary>
         public float LatestServerTime
         {
@@ -128,11 +127,12 @@ namespace JamesFrowen.PositionSync
             get => _clientDelay;
             set => _clientDelay = value;
         }
-
+		
+		// Used for debug purposes. Move along...
         public float DebugScale => clientScaleTime;
 
-        /// <param name="diffThreshold">how far off client time can be before changing its speed, Good value is half SyncInterval</param>
-        /// <param name="movingAverageCount">how many ticks used in average, increase or decrease with framerate</param>
+        /// <param name="diffThreshold">How far off client time can be before changing its speed. A good recommended value is half of SyncInterval.</param>
+        /// <param name="movingAverageCount">How many ticks are used for averaging purposes, you may need to increase or decrease with frame rate.</param>
         public InterpolationTime(float tickInterval, float diffThreshold = 0.5f, float timeScale = 0.01f, float skipThreshold = 2.5f, float tickDelay = 2, int movingAverageCount = 30)
         {
             positiveThreshold = tickInterval * diffThreshold;
@@ -146,12 +146,12 @@ namespace JamesFrowen.PositionSync
 
             diffAvg = new ExponentialMovingAverage(movingAverageCount);
 
-            // start at normal time scale
+            // Client should always start at normal time scale.
             clientScaleTime = normalScale;
         }
 
         /// <summary>
-        /// Updates client time
+        /// Updates the client time.
         /// </summary>
         /// <param name="deltaTime"></param>
         public void OnUpdate(float deltaTime)
@@ -165,11 +165,12 @@ namespace JamesFrowen.PositionSync
         /// <param name="serverTime"></param>
         public void OnMessage(float serverTime)
         {
-            logger.Assert(serverTime > _latestServerTime, $"Received message out of order server:{serverTime}, new:{_latestServerTime}");
+            logger.Assert(serverTime > _latestServerTime, $"Received message out of order. Server Time: {serverTime} vs New Time: {_latestServerTime}");
             _latestServerTime = serverTime;
 
-            // if first message set client time to server-diff
-            // reset stuff if too far behind
+			// If this is the first message, set the client time to the server difference.
+			// If we're too far behind, then we should reset things too.
+
             // todo check this is correct
             if (!intialized)
             {
@@ -177,23 +178,29 @@ namespace JamesFrowen.PositionSync
                 return;
             }
 
+			// Calculate the difference.
             float diff = serverTime - _clientTime;
 
+			// Are we falling behind?
             if (serverTime - _clientTime > _skipAheadThreshold)
             {
-                logger.LogWarning($"Client fell behind, skipping ahead. server:{serverTime:0.00} diff:{diff:0.00}");
+                logger.LogWarning($"Client fell behind, skipping ahead. Server Time: {serverTime:0.00}, Difference: {diff:0.00}");
                 InitNew(serverTime);
                 return;
             }
 
             diffAvg.Add(diff);
 
+			// Adjust the client time scale with the appropriate value.
             AdjustClientTimeScale((float)diffAvg.Value);
 
-            //todo add trace level
-            if (logger.LogEnabled()) logger.Log($"st {serverTime:0.00} ct {_clientTime:0.00} diff {diff * 1000:0.0}, wanted:{diffAvg.Value * 1000:0.0}, scale:{clientScaleTime}");
+            // todo add trace level
+            if (logger.LogEnabled()) logger.Log($"st: {serverTime:0.00}, ct: {_clientTime:0.00}, diff: {diff * 1000:0.0}, wanted: {diffAvg.Value * 1000:0.0}, scale: {clientScaleTime}");
         }
 
+        /// <summary>
+        /// Initializes and resets the system.
+        /// </summary>
         private void InitNew(float serverTime)
         {
             _clientTime = serverTime;
@@ -202,21 +209,23 @@ namespace JamesFrowen.PositionSync
             intialized = true;
         }
 
+        /// <summary>
+        /// A
+        /// </summary>
         private void AdjustClientTimeScale(float diff)
         {
-            // diff is server-client,
-            // if positive then server is ahead, => we can run client faster to catch up
-            // if negative then server is behind, => we need to run client slow to not run out of spanshots
+			// Difference is calculated between server and client.
+            // So if that difference is positive, we can run the client faster to catch up.
+			// However, if it's negative, we need to slow the client down otherwise we run out of snapshots.			
+			// Ideally, we want the difference vs the goal to be as close to 0 as possible.
 
-            // we want diffVsGoal to be as close to 0 as possible
-
-            // server ahead, speed up client
+            // Server's ahead of us, we need to speed up.
             if (diff > positiveThreshold)
-                clientScaleTime = fastScale;
-            // server behind, slow down client
+                clientScaleTime = fastScale;			
+            // Server is faling behind us, we need to slow down.
             else if (diff < negativeThreshold)
                 clientScaleTime = slowScale;
-            // close enough
+            // Server and client are on par ("close enough"). Run at normal speed.
             else
                 clientScaleTime = normalScale;
         }
