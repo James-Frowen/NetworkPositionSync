9bc4a4ea7495d5f812d6ebfc7d51c56675d2ac5d
James Frowen <jamesfrowendev@gmail.com>
2022-09-25 15:25:22 +0100

    chore(CI): fixing assembly Info path

--- .release.sh
+++ .release.sh
@@ -4,7 +4,7 @@ echo "Running release script with [SOURCE_PATH=${SOURCE_PATH}, TARGET_PATH=${TAR
 
 VER=$(echo $1 | sed 's/-[a-z]*//g')
 sed -i -e '/AssemblyVersion/s/\".*\"/\"'$VER'\"/' \
-    ${SOURCE_PATH}/AssemblyInfo.cs
+    ${SOURCE_PATH}/Runtime/AssemblyInfo.cs
 
 unity-packer pack NetworkPositionSync.unitypackage \
     ${SOURCE_PATH} ${TARGET_PATH}
