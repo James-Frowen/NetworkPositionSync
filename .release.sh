8abd97b91e8c3a1d9c67b985a44a179237454a75
James Frowen <jamesfrowendev@gmail.com>
2022-09-25 15:20:56 +0100

    chore: adding package.json, updating CI and changing repo layout

---
+++ .release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running release script with [SOURCE_PATH=${SOURCE_PATH}, TARGET_PATH=${TARGET_PATH}, args=$@]"
+
+VER=$(echo $1 | sed 's/-[a-z]*//g')
+sed -i -e '/AssemblyVersion/s/\".*\"/\"'$VER'\"/' \
+    ${SOURCE_PATH}/AssemblyInfo.cs
+
+unity-packer pack NetworkPositionSync.unitypackage \
+    ${SOURCE_PATH} ${TARGET_PATH}
