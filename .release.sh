27904364f1fbed0a5fe6a189163935720bc267d5
James Frowen <jamesfrowendev@gmail.com>
2022-09-25 17:12:12 +0100

    CI: fixing release sh

--- .release.sh
+++ .release.sh
@@ -7,8 +7,8 @@ sed -i -e '/AssemblyVersion/s/\".*\"/\"'$VER'\"/' \
     ${SOURCE_PATH}/Runtime/AssemblyInfo.cs
 
 unity-packer pack NetworkPositionSync.unitypackage \
-    ${SOURCE_PATH}/Runtime ${TARGET_PATH}/Runtime
-    ${SOURCE_PATH}/CHANGELOG.md ${TARGET_PATH}/CHANGELOG.md
-    ${SOURCE_PATH}/LICENSE ${TARGET_PATH}/LICENSE
-    ${SOURCE_PATH}/package.json ${TARGET_PATH}/package.json
+    ${SOURCE_PATH}/Runtime ${TARGET_PATH}/Runtime \
+    ${SOURCE_PATH}/CHANGELOG.md ${TARGET_PATH}/CHANGELOG.md \
+    ${SOURCE_PATH}/LICENSE ${TARGET_PATH}/LICENSE \
+    ${SOURCE_PATH}/package.json ${TARGET_PATH}/package.json \
     ${SOURCE_PATH}/Readme.txt ${TARGET_PATH}/Readme.txt
